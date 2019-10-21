85ad474a1e4c13bda0f3ea1102af37e59e36d2f7
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-10-21 14:45:05 +0200

    Update -> bgImage CDN

--- src/containers/sectionTwo.js
+++ src/containers/sectionTwo.js
@@ -5,7 +5,7 @@ import "../styles/styles.css";
 
 const SectionTwo = () => {
     return(
-        <Box className="sectionContainer" bgImage={ "url("+ sectionImage + ")" } bgRepeat="no-repeat" width="100%" height="700px" color="white" overflow="hidden">
+        <Box className="sectionContainer" bgImage={ "url(https://res.cloudinary.com/wemakeart/image/upload/v1571656529/pmb-placeholder/gogoa-final_mi1ry5.png)" } bgRepeat="no-repeat" width="100%" height="700px" color="white" overflow="hidden">
             <Content
                 title="GoGoa"
                 shortDesc="Our bursary division -_-"
