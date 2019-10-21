a24db519c6cf7139206dc0bed089a66ba69e1258
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-10-21 14:45:30 +0200

    Update -> font color & bgImage CDN

--- src/containers/sectionThree.js
+++ src/containers/sectionThree.js
@@ -5,7 +5,7 @@ import "../styles/styles.css";
 
 const SectionThree = () => {
     return(
-        <Box className="sectionContainer" bgImage={ "url("+ sectionImage + ")" } bgRepeat="no-repeat" width="100%" height="700px" color="white"  overflow="hidden">
+        <Box className="sectionContainer" bgImage={ "url(https://res.cloudinary.com/wemakeart/image/upload/v1571656526/pmb-placeholder/newsroom-final_oaegif.png)" } bgRepeat="no-repeat" width="100%" height="700px" color="#ed0677"  overflow="hidden">
             <Content
                 title="The Newsroom"
                 shortDesc="Stay up-to-date with everything PMB"
