06fa5c293e024ba5c0b0a01a70b62b176abc29ac
AmoDinho <u14284783@tuks.co.za>
2019-10-24 17:18:37 +0200

    Im done

--- src/App.js
+++ src/App.js
@@ -10,19 +10,30 @@ align-items: center;
 justify-items: center;
 grid-template-columns: repeat(auto-fit, minmax(320px,1fr));
 `
+
+const LinkStyles = styled.a`
+
+  text-decoration: none;
+color: black;
+
+`
 function App() {
   return (
     <div>
       <Hero />
       <CardGrid>
+        <LinkStyles href="https://gogoa.co.za">
         <Card
           heading="GOA"
           text="Our burasry management app"
         />
+        </LinkStyles>
+        <LinkStyles href="https://newsroom.pimpmybook.co.za">
         <Card
           heading="Newsroom"
           text="Our blog"
         />
+        </LinkStyles>
         <Card
           heading="PMB +"
           text="Forthcoming marketplace
