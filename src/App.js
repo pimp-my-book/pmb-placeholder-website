15c67debe8a173731a4ef8bdd96310c61ca084fe
AmoDinho <u14284783@tuks.co.za>
2019-10-24 17:14:12 +0200

    got it centered

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
+import styled from 'styled-components'
 import Hero from './components/Hero'
 import Card from './components/Card'
+
+const CardGrid = styled.div`
+display: grid;
+grid-gap:10px;
+align-items: center;
+justify-items: center;
+grid-template-columns: repeat(auto-fit, minmax(320px,1fr));
+`
 function App() {
   return (
     <div>
       <Hero />
-      <div>
+      <CardGrid>
         <Card
           heading="GOA"
           text="Our burasry management app"
         />
-      </div>
+        <Card
+          heading="Newsroom"
+          text="Our blog"
+        />
+        <Card
+          heading="PMB +"
+          text="Forthcoming marketplace
+          service"
+        />
+      </CardGrid>
     </div>
   );
 }
