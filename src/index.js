45241f89b566fa93f154a22b0a5844cc01358d75
AmoDinho <u14284783@tuks.co.za>
2019-10-24 16:25:56 +0200

    added google fonts

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createGlobalStyle } from 'styled-components'
 const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Kanit|Rubik+Mono+One&display=swap');
 
+h1,h2,h3 {
+    font-family: 'Rubik Mono One', sans-serif;
+}
+
+h4,p {
+    font-family: 'Kanit', sans-serif;
+}
 body{
   background: linear-gradient(90deg, #249DC9 2.49%, #F9B4ED 76.81%, #F9B4ED 87.91%);
 }
