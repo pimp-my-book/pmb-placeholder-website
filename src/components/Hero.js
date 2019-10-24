74386b9ac6d8733ad568854a1ab3f77fd3c0eb97
AmoDinho <u14284783@tuks.co.za>
2019-10-24 16:53:51 +0200

    it is working

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,9 +4,15 @@ import Logo from '../assets/pmb_logo.svg'
 
 const HeroGrid = styled.div`
 display: flex;
+flex-direction:column;
 align-items:center;
 justify-content: center;
 
+`
+
+const HeroHeading = styled.h1`
+color: white;
+text-align: center;
 `
 const Hero = () => {
     return (
@@ -14,6 +20,9 @@ const Hero = () => {
             <HeroGrid>
 
                 <img alt="PMB Logo" src={Logo} />
+                <HeroHeading>
+                    We are focused on making education accessible
+                </HeroHeading>
             </HeroGrid>
 
 
