7a312ab4d684fff4a938eb4b6afb336607ed5231
AmoDinho <u14284783@tuks.co.za>
2019-10-24 16:49:22 +0200

    got it centered

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
 import Logo from '../assets/pmb_logo.svg'
+
+const HeroGrid = styled.div`
+display: flex;
+align-items:center;
+justify-content: center;
+
+`
 const Hero = () => {
     return (
         <>
-            <img alt="PMB Logo" src={Logo} />
+            <HeroGrid>
+
+                <img alt="PMB Logo" src={Logo} />
+            </HeroGrid>
+
 
         </>
     )
