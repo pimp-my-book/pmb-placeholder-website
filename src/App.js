bdc4cb1725261ae48df3dc3192d07fa19f405fca
AmoDinho <u14284783@tuks.co.za>
2019-10-24 16:07:45 +0200

    added card.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import SectionOne from "./containers/sectionOne";
-import SectionTwo from "./containers/sectionTwo";
-import SectionThree from "./containers/sectionThree";
-import Footer from "./components/footer";
-import './styles/styles.css';
+
 
 function App() {
   return (
     <div>
-      <SectionOne />
-      <SectionTwo />
-      <SectionThree />
-      <Footer />
+      Home
     </div>
   );
 }
