af456f2c78decffb98d318ffb117994c5c5d5e40
AmoDinho <u14284783@tuks.co.za>
2019-10-24 17:05:19 +0200

    added animations

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,25 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+const float = keyframes`
+ from {
+     transform: translate(0, 0px);
+ }
+ 65% {
+     transform: translate(0, 15px);
+ }
+ to {
+     transform: translate(0, -0px);
+ }
+`
+
+const CardStyles = styled.div`
+width: 253px;
+height: 249px;
+background: white;
+border-radius:10px;
+animation: ${float} 5s infinite ease-in-out;
+`
 
 const Card = () => {
     return (
