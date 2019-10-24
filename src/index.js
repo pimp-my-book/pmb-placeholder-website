e274a833222c09b944407aa7f503f0493defff66
AmoDinho <u14284783@tuks.co.za>
2019-10-24 16:23:55 +0200

    added a global style

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/styles.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createGlobalStyle } from 'styled-components'
+const GlobalStyle = createGlobalStyle`
 
+body{
+  background: linear-gradient(90deg, #249DC9 2.49%, #F9B4ED 76.81%, #F9B4ED 87.91%);
+}
+`
 ReactDOM.render(
-    <App />,
+    <GlobalStyle>
+        <App />
+    </GlobalStyle>,
     document.getElementById('root')
 );
 
