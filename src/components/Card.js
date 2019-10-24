1da922c0aafc9fe88e04c59cd1c1b5c54d04f29f
AmoDinho <u14284783@tuks.co.za>
2019-10-24 17:02:15 +0200

    added card skeleton

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Card = () => {
+    return (
+        <>
+
+        <>
+                )
+            }
+            
+export default Card
